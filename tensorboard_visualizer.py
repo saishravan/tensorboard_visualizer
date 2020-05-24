439d9c00423180337623d656addc5ad54ec6940d
saishravan <saishravan.reddy@gmail.com>
2020-05-24 12:07:53 +0530

    adding files

---
+++ tensorboard_visualizer.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+a = tf.add(1, 2, name="Add_these_numbers")
+b = tf.multiply(a, 3)
+c = tf.add(4, 5, name="And_These_ones")
+d = tf.multiply(c, 6, name="Multiply_these_numbers")
+e = tf.multiply(4, 5, name="B_add")
+f = tf.div(c, 6, name="B_mul")
+g = tf.add(b, d)
+h = tf.multiply(g, f)
+i = h
+j = i+g
+
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter("output", sess.graph)
+    print(sess.run(i))
+    writer.close()
